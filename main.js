7cc95f699dd8e607dded1d3d4f38d61771cc757a
wrs22n <wrs22n@gmail.com>
2025-02-27 11:30:45 +0200

    update

--- main.js
+++ main.js
@@ -102,5 +102,5 @@ app.post('/api/persons', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running`);
+    console.log(`Server running on port ${PORT}`);
 });
